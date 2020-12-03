b7ee381faac34b8ef18a58c88c1fdf255dc286f7
Calvin <calvinminhla@gmail.com>
2020-12-02 20:55:21 -0800

    Properly refactored code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-var App = () => (
+import VideoList from './VideoList.js';
+
+var App = (props) => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
@@ -9,8 +11,8 @@ var App = () => (
       <div className="col-md-7">
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+      <div id="videoList" className="col-md-5">
+        <VideoList videos={props.data}/>
       </div>
     </div>
   </div>
