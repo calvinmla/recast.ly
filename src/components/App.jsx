41e019425350747225818b50919c888b2bb04421
Calvin <calvinminhla@gmail.com>
2020-12-03 10:35:13 -0800

    Complete creating a dynamic Video List component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,12 @@ var App = (props) => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em>search</em>view goes here</h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em>view goes here</h5></div>
       </div>
       <div id="videoList" className="col-md-5">
         <VideoList videos={props.data}/>
