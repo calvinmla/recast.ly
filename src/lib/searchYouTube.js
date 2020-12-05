d8768a8c7995f38b0bf5bf069f55fd59a62b7da2
Calvin <calvinminhla@gmail.com>
2020-12-05 11:50:20 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,9 @@
+import YOUTUBE_API_KEY from '/compiled/src/config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+// TODO
+  $.get('https://www.googleapis.com/youtube/v3/search', options, callback);
 };
+
 
 export default searchYouTube;
